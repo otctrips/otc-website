e2c4810f6751c7acccd0836ecc2dbdf6d514816c
otctrips <tdmvofficial@otctrips.com>
2026-09-16 16:13:38 -0400

    update about page story

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import Image from "next/image";
+
 import PageHero from "@/components/PageHero";
 import FadeIn from "@/components/FadeIn";
-import { IMAGES } from "@/lib/data";
+
 
 export const metadata: Metadata = {
   title: "About",
